bc14e737882baa35200ff6bf2a017375d6e41e55
strudelsnft <103766813+strudelsnft@users.noreply.github.com>
2024-01-31 17:46:54 -0800

    Update constants.ts

--- apps/platform/src/constants.ts
+++ apps/platform/src/constants.ts
@@ -1,6 +1,6 @@
 import { GambaStandardTokens, TokenMeta } from 'gamba-react-ui-v2'
 
-export const PLATFORM_CREATOR_ADDRESS = 'V2grJiwjs25iJYqumbHyKo5MTK7SFqZSdmoRaj8QWb9'
+export const PLATFORM_CREATOR_ADDRESS = 'BT8CiUWnUBJMYorZngm5rkKWikm9F8MTQkj1SueJc4qt'
 
 /** Appears in ShareModal */
 export const PLATFORM_SHARABLE_URL = 'v2-play.gamba.so'
