b96be8715fca8f1974f3c35e619123221228c71f
strudelsnft <103766813+strudelsnft@users.noreply.github.com>
2024-01-31 20:19:21 -0800

    Update constants.ts

--- apps/platform/src/constants.ts
+++ apps/platform/src/constants.ts
@@ -9,4 +9,12 @@ export const PLATFORM_SHARABLE_URL = 'v2-play.gamba.so'
 export const TOKENS: TokenMeta[] = [
   GambaStandardTokens.sol,
   GambaStandardTokens.usdc,
+{
+    mint: new PublicKey("JUPyiwrYJFskUPiHa7hkeR8VUtAeFoSYbKedZNsDvCN"),
+    symbol: 'JUP',
+    name: 'Jupiter',
+    image: "https://assets.coingecko.com/coins/images/34188/standard/jup.png?1704266489",
+    decimals: 6,
+    baseWager: 1 * 1e6,
+  }
 ]
